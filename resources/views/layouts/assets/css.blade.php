7df27870e48f63b8d94710818874f4e63c438dd3
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2024-02-23 14:54:13 +0200

    update

--- resources/views/layouts/assets/css.blade.php
+++ resources/views/layouts/assets/css.blade.php
@@ -19,4 +19,5 @@
 <link id="color" rel="stylesheet" href="{{asset('assets/css/color-1.css')}}" media="screen">
 <!-- Responsive css-->
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsive.css')}}">
-
+<!-- Custom css-->
+<link rel="stylesheet" href={{asset('assets/css/custom.css')}}>
