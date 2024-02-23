c03ca3fe3b8551ecefeba773e47023cb17dfb652
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2024-02-23 15:14:39 +0200

    update

--- resources/views/layouts/assets/css.blade.php
+++ resources/views/layouts/assets/css.blade.php
@@ -1,23 +1,28 @@
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.css')}}">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fontawesome-6-pro@6.4.0/css/all.min.css"/>
 <!-- ico-font-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/icofont.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/icofont.css')}}">
 <!-- Themify icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/themify.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/themify.css')}}">
 <!-- Flag icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/flag-icon.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/flag-icon.css')}}">
 <!-- Feather icon-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/feather-icon.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/feather-icon.css')}}">
 <!-- Plugins css start-->
 @yield('css')
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/slick.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/slick-theme.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/scrollbar.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/slick.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/slick-theme.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/scrollbar.css')}}">
 <!-- Bootstrap css-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/bootstrap.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/bootstrap.css')}}">
 <!-- App css-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 <link id="color" rel="stylesheet" href="{{asset('assets/css/color-1.css')}}" media="screen">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 <!-- Responsive css-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsive.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+{{-- ! Datatables ! --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+{{-- ! Date Picker ! --}}
+<link rel="stylesheet" href="{{asset('assets/css/vendors/date-picker.css')}}">
 <!-- Custom css-->
 <link rel="stylesheet" href={{asset('assets/css/custom.css')}}>
