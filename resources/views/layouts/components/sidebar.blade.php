ec6273eb381d003b91470056d80d2e9a3a4bb75a
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2024-02-24 13:28:26 +0200

    update

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -2,69 +2,125 @@
     <div>
         <div class="logo-wrapper">
             <a href="{{ route('home') }}">
-                <img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt="">
-                <img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt="">
+                <h3>جمعية أدندان الخيرية</h3>
             </a>
-            <div class="back-btn">
-                <i class="fa fa-angle-left"></i>
-            </div>
             <div class="toggle-sidebar">
                 <i class="status_toggle middle sidebar-toggle" data-feather="grid"></i>
             </div>
         </div>
-        <div class="logo-icon-wrapper">
-            <a href="{{ route('home') }}">
-                <img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon.png') }}" alt="">
+        <div class="logo-icon-wrapper text-center mt-3 mb-5 mx-3 p-0">
+            <a href={{route('home')}}>
+                <img class="img-fluid me-0" src="{{ asset('assets/images/logo/لوجو الجمعية.png') }}" width="70" alt="Logo">
             </a>
         </div>
         <nav class="sidebar-main">
-            <div class="left-arrow" id="left-arrow">
-                <i data-feather="arrow-left"></i>
-            </div>
             <div id="sidebar-menu">
-                <ul class="sidebar-links" id="simple-bar">
-                    <li class="back-btn">
-                        <div class="mobile-back text-end">
-                            <span>Back</span>
-                            <i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
-                        </div>
-                    </li>
-                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ route('home') }}" target="_blank">
+                <ul class="sidebar-links w-100" id="simple-bar">
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="{{ route('home') }}" target="_blank">
                             <svg class="stroke-icon">
                                 <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-home') }}"></use>
                             </svg>
-                            <svg class="fill-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#fill-home') }}"></use>
-                            </svg>
-                            <span>Dashboard</span>
+                            <span>لوحة التحكم</span>
                         </a>
                     </li>
                     <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title" href="https://pixelstrap.freshdesk.com/support/home" target="_blank">
-                            <svg class="stroke-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-social') }}"></use>
-                            </svg>
-                            <svg class="fill-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#fill-social') }}"></use>
-                            </svg>
-                            <span>Raise Support</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title" href="https://docs.pixelstrap.com/cuba/laravel/document/" target="_blank">
+                        <a class="sidebar-link sidebar-title" href="" target="_blank">
                             <svg class="stroke-icon">
                                 <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-form') }}"></use>
                             </svg>
-                            <svg class="fill-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#fill-form') }}"></use>
+                            <span>الأخبار </span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="" target="_blank">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#fill-user') }}"></use>
                             </svg>
-                            <span>Documentation </span>
+                            <span>الإشتراكات</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="sidebar-list">
+                        <a href="" class="sidebar-link sidebar-title">
+                            <svg class="fill-icon">
+                                <use></use>
+                            </svg>
+                            <span></span>
                         </a>
                     </li>
                 </ul>
-            </div>
-            <div class="right-arrow" id="right-arrow">
-                <i data-feather="arrow-right"></i>
             </div>
         </nav>
     </div>
