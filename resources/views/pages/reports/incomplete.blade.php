74839cd4378a1ec688ac2f7c36a65f46288ba5fd
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2024-07-20 23:47:38 +0300

    update

--- resources/views/pages/reports/incomplete.blade.php
+++ resources/views/pages/reports/incomplete.blade.php
@@ -19,8 +19,41 @@
                         <div class="card-content text-center">
                             <h3 class="text-center text-decoration-underline mb-4">الرقم لقومي</h3>
                             <p class="mb-3 fw-bold">{{$incompleteSSNCount}}</p>
-                            <button class="btn btn-primary fw-bold w-100 fs-5">المزيد</button>
-                            <div class="fade modal"></div>
+                            <button class="btn btn-primary fw-bold w-100 fs-5" type="button" data-bs-toggle="modal" data-bs-target="#ssn">المزيد</button>
+                            <div class="fade modal" id="ssn" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-centered-dialog modal-fullscreen">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h3 class="modal-title fs-5 text-white" id="exampleModalLabel">بيانات الرقم القومي</h3>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="table-responsive">
+                                                <table class="table display align-middle text-center table-hover" id="table35" data-order='[[ 0, "asc" ]]' data-page-length='10'>
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="text-center">رقم العضوية</th>
+                                                            <th class="text-center">إسم العضو</th>
+                                                            <th class="text-center">رقم القومي</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($incompleteSSN as $ssn)
+                                                            <tr>
+                                                                <td>{{$ssn->member_id}}</td>`
+                                                                <td>{{$ssn->name}}</td>
+                                                                <td>{{$ssn->ssn}}</td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">إلغاء</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -34,8 +67,33 @@
                         <div class="card-content text-center">
                             <h3 class="text-center text-decoration-underline mb-4">رقم المحمول</h3>
                             <p class="mb-3 fw-bold">{{$incompleteMobileCount}}</p>
-                            <button class="btn btn-primary fw-bold w-100 fs-5">المزيد</button>
-                            <div class="fade modal"></div>
+                            <button class="btn btn-primary fw-bold w-100 fs-5" type="button" data-bs-toggle="modal" data-bs-target="#mobile">المزيد</button>
+                            <div class="fade modal" id="mobile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-centered-dialog modal-fullscreen">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h3 class="modal-title fs-5 text-white" id="exampleModalLabel">بيانات رقم المحمول</h3>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="table-responsive">
+                                                <table class="table display align-middle text-center table-hover" id="table36" data-order='[[ 0, "asc" ]]' data-page-length='10'>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>رقم العضوية</th>
+                                                            <th>إسم العضو</th>
+                                                            <th>رقم المحمول</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody></tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">إلغاء</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -49,8 +107,33 @@
                         <div class="card-content text-center">
                             <h3 class="text-center text-decoration-underline mb-4">العنوان</h3>
                             <p class="mb-3 fw-bold">{{$incompleteAddressCount}}</p>
-                            <button class="btn btn-primary fw-bold w-100 fs-5">المزيد</button>
-                            <div class="fade modal"></div>
+                            <button class="btn btn-primary fw-bold w-100 fs-5" type="button" data-bs-toggle="modal" data-bs-target="#address">المزيد</button>
+                            <div class="fade modal" id="address" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-centered-dialog modal-fullscreen">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h3 class="modal-title fs-5 text-white" id="exampleModalLabel">بيانات العنوان</h3>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="table-responsive">
+                                                <table class="table display align-middle text-center table-hover" id="table37" data-order='[[ 0, "asc" ]]' data-page-length='10'>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>رقم العضوية</th>
+                                                            <th>إسم العضو</th>
+                                                            <th>العنوان</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody></tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">إلغاء</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
