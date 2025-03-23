17821927ef8aee349148092db916e8197c96acea
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2025-03-24 01:30:22 +0200

    update

---
+++ config/paymob.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'api_key' => env('PAYMOB_API_KEY'),
+    'integration_id' => env('PAYMOB_INTEGRATION_ID'),
+    'iframe_id' => env('PAYMOB_IFRAME_ID'),
+    'hmac_secret' => env('PAYMOB_HMAC_SECRET'),
+    'intention_url' => 'https://accept.paymob.com/v1/intention/',
+];
