ae4ca9200189e2a9cc61685fe5faf5041b295bdd
Omar ElNoby <omaraboregela20112011@hotmail.com>
2025-08-04 12:54:07 +0300

    update some packages and fix share buttons

--- resources/views/frontend/layouts/assets/css.blade.php
+++ resources/views/frontend/layouts/assets/css.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fontawesome-6-pro@6.4.0/css/all.min.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/aquawolf04/font-awesome-pro@5cd1511/css/all.css"/>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 {{-- <link rel="stylesheet" href={{asset('assets/frontend/css/lib/bootstrap.css')}}> --}}
