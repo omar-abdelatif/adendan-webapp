b52978ae548986f226f5231bee02b8481313dfd8
Omar ElNoby <omaraboregela20112011@hotmail.com>
2025-11-06 14:12:23 +0200

    update

--- resources/views/frontend/pages/news/single_news.blade.php
+++ resources/views/frontend/pages/news/single_news.blade.php
@@ -31,7 +31,7 @@
                     <div class="col-lg-12">
                         <div class="mb-5">
                             {{-- <img loading="lazy" class="rounded-5 w-100 h-auto" height="500" src="{{asset('assets/images/news-imgs/'.$news->img)}}" alt="{{$news->title}}"> --}}
-                            <div class="rounded-5" style="width: 100%; height: 500px; background-image: url({{ asset('assets/images/news-imgs/'.$news->img) }}); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
+                            <div class="rounded-5" style="width: 100%; height: 600px; background-image: url({{ asset('assets/images/news-imgs/'.$news->img) }}); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
                         </div>
                     </div>
                 @else
